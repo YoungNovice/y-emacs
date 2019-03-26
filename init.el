0054045e55fb9c5d4465acfa48bbc31daf4c070a
AlanYoung <2306915539@qq.com>
2019-03-27 00:23:35 +0800

    add init-packages.el file

--- init.el
+++ init.el
@@ -1,35 +1,12 @@
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t))
-
-(require 'cl)
-(defvar young/packages '(
-			 company
-			 monokai-theme
-			 zenburn-theme
-			 hungry-delete
-			 smex
-			 counsel
-			 swiper
-			 smartparens
-			 js2-mode
-			 nodejs-repl
-			 exec-path-from-shell
-			 ) "Default packages")
-
-
-(defun young/packages-installed-p ()
-  (loop for pkg in young/packages
-	when (not (package-installed-p pkg)) do (return nil)
-	finally (return t)))
-
-(unless (young/packages-installed-p)
-				   (message "%s" "Refreshing package database..." )
-				   (package-refresh-contents)
-				   (dolist (pkg young/packages)
-				     (when (not (package-installed-p pkg))
-				       (package-install pkg))))
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'init-packages)
+;; call load but can't why we should baozheng init.package.el file in load-path
+;; use add-to-list 'load-path too include our el configurations
 
 (global-set-key (kbd "C-h C-f") 'find-function)
 (global-set-key (kbd "C-h C-v") 'find-variable)
@@ -38,17 +15,8 @@
 (setq org-agenda-files '("~/org"))
 (global-set-key (kbd "C-c a") 'org-mode)
 
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
 (require 'nodejs-repl)
-(require 'smartparens-config)
 ;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(smartparens-global-mode t)
-
-(setq auto-mode-alist (append  '(("\\.js\\'" . js2-mode)) auto-mode-alist))
-
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
@@ -57,27 +25,27 @@
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "<f1> f") 'counsel-describe-function)
 (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
-
-
-(require 'smex)
-(smex-initialize)
+;;(require 'smex)
+;;(smex-initialize)
 ;;(global-set-key (kbd "M-x") 'smex)
 
 (setq package-selected-packages young/packages)
-(require 'hungry-delete)
-(global-hungry-delete-mode 1)
+
 (global-auto-revert-mode 1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (global-linum-mode t)
 (setq inhibit-splash-screen t)
 
+(abbrev-mode t)
+(define-abbrev-table 'global-abbrev-table '(("8yx" "yangxuan") ("8github" "git@github.com:YoungNovice")))
+
 (defun open-my-init-file()
   (interactive)
   (find-file "~/.emacs.d/init.el"))
 
 (global-set-key (kbd "<f2>") 'open-my-init-file)
-(global-company-mode t)
+
 ;;(setq-default cursor-type 'bar)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
@@ -95,7 +63,7 @@
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 (global-hl-line-mode t)
-(load-theme 'zenburn t)
+
 
 
 ;;copy region or whole line
@@ -140,3 +108,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(setq ring-bell-function 'ignore)
---
+++ lisp/init-packages.el
@@ -0,0 +1,55 @@
+(require 'cl)
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t))
+
+(defvar young/packages '(
+			 company
+			 popwin
+			 monokai-theme
+			 zenburn-theme
+			 hungry-delete
+;;			 smex
+			 counsel
+			 swiper
+			 smartparens
+			 js2-mode
+			 nodejs-repl
+			 exec-path-from-shell
+			 ) "Default packages")
+
+(defun young/packages-installed-p ()
+  (loop for pkg in young/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (young/packages-installed-p)
+				   (message "%s" "Refreshing package database..." )
+				   (package-refresh-contents)
+				   (dolist (pkg young/packages)
+				     (when (not (package-installed-p pkg))
+				       (package-install pkg))))
+
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(require 'hungry-delete)
+(global-hungry-delete-mode 1)
+
+(require 'smartparens-config)
+(smartparens-global-mode t)
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+
+(setq auto-mode-alist (append  '(("\\.js\\'" . js2-mode)) auto-mode-alist))
+
+(global-company-mode t)
+
+(load-theme 'monokai t)
+
+(require 'popwin)
+(popwin-mode t)
+
+(provide 'init-packages)
